3af69ba14f0f135ca7a2b60479a9c08ceb2e4380
Truong Nguyen <samuraitruong@hotmail.com>
2025-08-22 18:25:58 +1000

    style: adjust padding in search results section for improved layout
    - Reduced padding in the search results section from `py-5 px-5` to `py-2 px-2` for a more compact design.
    - Aimed at enhancing the overall user experience by optimizing space usage.

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -48,7 +48,7 @@ export default async function SearchPage({ searchParams }: SearchPageProps) {
           ]}
         />
 
-        <div className="search-results-section-area py-5 px-5">
+        <div className="search-results-section-area py-2 px-2">
           <div className="container">
             <div className="row">
               <div className="col-lg-12">
